0670d200e95c935307e2c582fa0ecfcc0459999d
Kanokpit <thana.drnn@hotmail.com>
2025-09-19 08:30:27 +0700

    fix: support Thai language

--- src/utils/server/generate-contract-html.ts
+++ src/utils/server/generate-contract-html.ts
@@ -8,9 +8,10 @@ export const generateContractHTML = async (data: ContractData): Promise<string> 
 		const componentHTML = renderToStaticMarkup(React.createElement(ContractTemplate, { data }));
 
 		return `<!DOCTYPE html>
-    <html>
+    <html lang="th">
     <head>
       <meta charset="UTF-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
       <title>สัญญาการจ้างงาน</title>
       <style>
         @page {
